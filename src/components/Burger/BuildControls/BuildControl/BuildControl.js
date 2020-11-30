eb1e7800d1f275dcfb53478a1cd9262d7c21fa94
AhamedBilal <bilalazmy0@gmail.com>
2020-12-01 00:13:31 +0530

    Build Controls Dynamic

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './BuildControls.module.css'
 const BuildControl = (props) => (
     <div className={classes.BuildControl}>
-        <div className={classes.Label}>{props.lable}</div>
+        <div className={classes.Label}>{props.label}</div>
         <button className={classes.Less}>Remove</button>
         <button className={classes.More}>Add</button>
     </div>
